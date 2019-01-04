2f33a552872c0b08f71c2dbb945f4a8c3a88aaf7
thais <t.avelino@gmail.com>
2019-01-04 05:22:01 +0100

    finishing - need to check merge problems maybe

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,15 @@ DEPLOY_APACHE_ROOT="/var/www"
 DEPLOY_ROOT_THIS="${DEPLOY_APACHE_ROOT}/cv"
 DEPLOY_BRANCH="$(git branch | grep "*" | awk '{ print $2}')"
 DEPLOY_COMMAND="$(git push origin ${DEPLOY_BRANCH})"
-DEPLOY_STATUS="$(git status | grep modified | cut -f2 | awk '{print $1}')"
+DEPLOY_STATUS_MOD="$(git status | grep modified | cut -f2 | awk '{print $1}')"
+DEPLOY_STATUS_PUSH="$(git status | grep up-to-date | awk '{ print $4}')"
 
 MY_IP="$(ip addr show enp0s3 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
 echo && echo "Welcome '$(hostname -f)' ip: ${MY_IP}" && echo "Start Pushing work at $(date)"
 
 echo "Pushing to	'PROD'" && echo "at Branch	'${DEPLOY_BRANCH}'" && echo "of user		'${DEPLOY_USR_NAME}'" && echo "project 	'${DEPLOY_ROOT_THIS}'"
 
-if [ "${DEPLOY_STATUS}" = "modified:" ]; then
+if [ "${DEPLOY_STATUS_MOD}" = "modified:" ]; then
 	echo && echo "*************************************"
 	echo "make sure you did git add and git commit "
 	echo "*************************************" && echo
@@ -18,14 +19,24 @@ if [ "${DEPLOY_STATUS}" = "modified:" ]; then
 	exit 1
 fi
 
+if [ "${DEPLOY_STATUS_PUSH}" = "up-to-date" ]; then
+	echo ${DEPLOY_COMMAND}
+	echo && echo "SUCESS Push has finished at $(date)"
+	echo "*** SUCESS ***"
+	echo "See you soon '$(hostname -f)' =)"
+	exit 1
+fi
+
 if [ "${DEPLOY_COMMAND}" = "Everything up-to-date" ]; then
 	echo "Nothing to push"
+	echo "*** SUCESS ***"
+	echo "See you soon '$(hostname -f)' =)"
+	exit 1
 fi
 
 if [ "${DEPLOY_COMMAND}" != "Everything up-to-date" ]; then
 	echo ${DEPLOY_COMMAND}
 	echo && echo "SUCESS Push has finished at $(date)"
+	echo "*** SUCESS ***"
+	echo "See you soon '$(hostname -f)' =)"
 fi
-
-echo "See you soon '$(hostname -f)' =)"
-
