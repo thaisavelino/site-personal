f497b371a061012f66cd8c9858c751a5ca0d9848
thais <t.avelino@gmail.com>
2019-01-04 05:36:06 +0100

    trying to fix

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 MY_IP="$(ip addr show enp0s3 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
-echo && echo "Welcome '$(hostname -f)' ip: ${MY_IP}" && echo "Start Pushing work at $(date)"
+echo && echo "Welcome '$(hostname -f)' ip: ${MY_IP}" && echo "Start Pushing work at $(date)" && echo
 
 DEPLOY_USR_NAME="$(whoami)"
 DEPLOY_APACHE_ROOT="/var/www"
 DEPLOY_ROOT_THIS="${DEPLOY_APACHE_ROOT}/cv"
 DEPLOY_BRANCH="$(git branch | grep "*" | awk '{ print $2}')"
+DEPLOY_STATUS_MOD="$(git status | grep modified | cut -f2 | awk '{print $1}')"
+DEPLOY_STATUS_PUSH="$(git status | grep up-to-date | awk '{print $4}')"
 
 echo "Pushing to	'PROD'" && echo "at Branch	'${DEPLOY_BRANCH}'" && echo "of user		'${DEPLOY_USR_NAME}'" && echo "project 	'${DEPLOY_ROOT_THIS}'"
 
-DEPLOY_STATUS_MOD="$(git status | grep modified | cut -f2 | awk '{print $1}')"
-DEPLOY_COMMAND="$(git push origin ${DEPLOY_BRANCH})"
 
 if [ "${DEPLOY_STATUS_MOD}" = "modified:" ]; then
 	echo && echo "*************************************"
@@ -20,7 +20,7 @@ if [ "${DEPLOY_STATUS_MOD}" = "modified:" ]; then
 	exit 1
 fi
 
-DEPLOY_STATUS_PUSH="$(git status | grep up-to-date | awk '{ print $4}')"
+DEPLOY_COMMAND="$(git push origin ${DEPLOY_BRANCH})"
 
 if [ "${DEPLOY_STATUS_PUSH}" = "up-to-date" ]; then
 	echo ${DEPLOY_COMMAND}
