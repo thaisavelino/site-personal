20b166093c6703732595228c69165c9be2011a2e
thais <t.avelino@gmail.com>
2019-01-04 04:58:36 +0100

    good deploy need small fixing

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
-export DEPLOY_USR_NAME='whoami'
-export DEPLOY_APACHE_ROOT="/var/www"
-export DEPLOY_ROOT_THIS="${DEPLOY_APACHE_ROOT}/cv"
-export DEPLOY_BRANCH='git branch'
+DEPLOY_USR_NAME="$(whoami)"
+DEPLOY_APACHE_ROOT="/var/www"
+DEPLOY_ROOT_THIS="${DEPLOY_APACHE_ROOT}/cv"
+DEPLOY_BRANCH="$(git branch | grep "*" | awk '{ print $2}')"
+DEPLOY_COMMAND="$(git push origin ${DEPLOY_BRANCH})"
+DEPLOY_STATUS-"$(git status | grep modified)"
 
 MY_IP="$(ip addr show enp0s3 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
 echo && echo "Welcome '$(hostname -f)' ip: ${MY_IP}" && echo "Start Pushing work at $(date)"
 
-echo "Pushing to prod at Branch '${DEPLOY_BRANCH}' of '${DEPLOY_USR_NAME}' project '${DEPLOY_ROOT_THIS}'"
+echo "Pushing to	'PROD'" && echo "at Branch	'${DEPLOY_BRANCH}'" && echo "of user		'${DEPLOY_USR_NAME}'" && echo "project 	'${DEPLOY_ROOT_THIS}'"
 
-echo && echo "********************************" && echo "Push finished at $(date)"
+if [ DEPLOY_STATUS ]; then
+	echo && echo "*************************************"
+	echo "make sure you did git add and git commit "
+	echo "*************************************" && echo
+fi
+
+if [ "${DEPLOY_COMMAND}" == 'Everything up-to-date' ]; then
+	echo "Nothing to push"
+fi
+echo && echo "SUCESS Push has finished at $(date)"
 echo "See you soon '$(hostname -f)' =)"
